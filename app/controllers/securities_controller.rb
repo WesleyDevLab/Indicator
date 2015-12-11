1ed5ee2612fecaa3f1cc65de0e6a390cb1665624
kenwmak <kenwmak@gmail.com.com>
2015-12-11 18:26:08 +0000

    Added more of the support resistance method

--- app/controllers/securities_controller.rb
+++ app/controllers/securities_controller.rb
@@ -7,50 +7,17 @@ class SecuritiesController < ApplicationController
     security = Security.new
     indicator = Indicator.new
     symbol = params[:symbol]
-    @quote = security.get_quote(symbol)
-    
-    history = security.get_history(symbol, 60)
 
+    @quote = security.get_quote(symbol)
+    history = security.get_history(symbol, 60)
     indicators = security.get_indicators(history)
+    
     @sma = indicator.get_ema(indicators, 5)
     @bb = indicator.get_bb(indicators, 15, 3)
     @macd = indicator.get_macd(indicators, 12, 26, 9)
     @rsi = indicator.get_rsi(indicators, 29)
     @sto = indicator.get_sto(indicators, 14, 3, 5)
-  end
 
-  def support_resistance
-
-    # Get historical quotes
-    history_arr = Securities::Stock.new(:symbol => "goog", :start_date => 120.day.ago.to_s)
-
-    # Get the prices into one array
-    price_arr = []
-    history_arr.output.each do |quote|
-      price_arr.push(quote[:close].to_f)
-    end
-
-    # Break timeseries into segments of N prices
-    price_segment_arr = []
-    price_arr.each_slice(10) {|price| price_segment_arr.push(price)}
-
-    # Find min of each segments
-    price_min_arr = []
-    price_segment_arr.each do |segment|
-      price_min_arr.push(segment.min)
-    end
-    price_min = price_min_arr.min
-
-    # 
-    price_support_arr = []
-    price_support_arr.push(price_min)
-    price_min_arr.each do |price|
-      if (price / price_min - 1).abs * 100 < 3
-        price_support_arr.push(price)
-      end
-    end
-
-    price_support_arr.sum / price_support_arr.size
-
+    @sup_resist = indicator.get_sup_resist(symbol, 120, 10, 1)
   end
 end
--- app/models/indicator.rb
+++ app/models/indicator.rb
@@ -12,8 +12,8 @@ class Indicator < ActiveRecord::Base
     indicators.calc(:type => :bb, :params => [period, multiplier]).output.last
   end
 
-  def get_macd(indicators, fast_period, slow_period, signal_line)
-    @macd = indicators.calc(:type => :macd, :params => [fast_period, slow_period, signal_line]).output.last
+  def get_macd(indicators, fast, slow, signal)
+    @macd = indicators.calc(:type => :macd, :params => [fast, slow, signal]).output.last
   end
   
   def get_rsi(indicators, period)
@@ -23,4 +23,43 @@ class Indicator < ActiveRecord::Base
   def get_sto(indicators, lookback, slow_k, d_ma) 
     @sto = indicators.calc(:type => :sto, :params => [lookback, slow_k, d_ma]).output.last
   end
+
+  ### Work in Progress
+  def get_sup_resist(period, num, pct)
+    security = Security.new
+
+    # Get historical quotes
+    history_arr = security.get_history(symbol, period)
+
+    # Get the prices into one array
+    price_arr = []
+    history_arr.output.each do |quote|
+      price_arr.push(quote[:close].to_f)
+    end
+
+    # Break timeseries into segments of N prices
+    price_segment_arr = []
+    price_arr.each_slice(num) {|price| price_segment_arr.push(price)}
+
+    # Find min of each segments
+    price_min_arr = []
+    price_segment_arr.each do |segment|
+      price_min_arr.push(segment.min)
+    end
+    price_min = price_min_arr.min
+
+    # Support is the min price and prices within pct of it
+    price_support_arr = []
+    price_support_arr.push(price_min)
+    price_min_arr.each do |price|
+      if (price / price_min - 1).abs * 100 < pct
+        price_support_arr.push(price)
+      end
+    end
+
+    # support price and strength
+    support =price_support_arr.sum / price_support_arr.size
+    support_strength = price_support_arr.size
+  end
+
 end
